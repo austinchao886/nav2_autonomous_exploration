4fab3f6c0a874443f4fe8425cb577844d917c07a
austin <worldhasntseenanything@gmail.com>
2025-11-26 18:02:33 +0800

    Add frontier clustering and filtering module to improve exploration stability

--- custom_explorer/explorer_cost_function.py
+++ custom_explorer/explorer_cost_function.py
@@ -10,8 +10,8 @@ from omegaconf import OmegaConf
 from ament_index_python.packages import get_package_share_directory
 import os
 from collections import deque
-
-
+from visualization_msgs.msg import Marker, MarkerArray
+import geometry_msgs.msg
 class ExplorerNode(Node):
     def __init__(self):
         super().__init__('explorer')
@@ -21,6 +21,14 @@ class ExplorerNode(Node):
         package_share = get_package_share_directory('custom_explorer')
         config_path = os.path.join(package_share, 'config', 'config.yaml')
         self.config = OmegaConf.load(config_path)
+        
+        # Frontier clustering & filtering parameters
+        self.min_cluster_size = 3  # Minimum size of frontier cluster to consider
+        self.obstacle_ratio_thresh = 0.5  
+        self.rho_threshold = 0.2      
+        self.patch_radius = 3         
+
+        self.current_target = None     
 
         # Subscriber to the map topic
         self.map_sub = self.create_subscription(
@@ -37,6 +45,13 @@ class ExplorerNode(Node):
         
         self.prev_odom = None
         self.total_distance = 0.0
+
+        # Publisher for frontier visualization
+        self.frontier_pub = self.create_publisher(
+            MarkerArray,
+            'frontier_markers',
+            10
+        )
 
         # Action client for navigation
         self.nav_to_pose_client = ActionClient(
@@ -57,6 +72,24 @@ class ExplorerNode(Node):
 
         # Get Distance and Time Information
         self.print_information_timer = self.create_timer(5.0, self.print_information)
+
+    def world_to_grid(self, x, y):
+        res = self.map_data.info.resolution
+        ox  = self.map_data.info.origin.position.x
+        oy  = self.map_data.info.origin.position.y
+        c = int((x - ox) / res)
+        r = int((y - oy) / res)
+        return (r, c)
+    
+    def grid_to_world(self, r, c):
+        """Convert grid index (row, col) -> world (x, y) in map frame."""
+        res = self.map_data.info.resolution
+        ox  = self.map_data.info.origin.position.x
+        oy  = self.map_data.info.origin.position.y
+        x = c * res + ox
+        y = r * res + oy
+        return x, y
+
 
     def odom_callback(self, msg): # msg is respect to world frame
         if self.prev_odom is None:
@@ -80,13 +113,94 @@ class ExplorerNode(Node):
         self.total_distance += distance
         self.prev_odom = msg
 
-        self.robot_position = (msg.pose.pose.position.x, msg.pose.pose.position.y)
+        if self.current_target and self.map_data is not None:
+            fr = self.current_target  # (row, col)
+            tx, ty = self.grid_to_world(fr[0], fr[1])  # 注意順序 r=row, c=col
+            dist = np.hypot(
+                msg.pose.pose.position.x - tx,
+                msg.pose.pose.position.y - ty
+            )
+            if dist < 0.3:
+                self.visited_frontiers.add(fr)
+                self.current_target = None
+
+        if self.map_data is None:
+            return
+        
+        self.robot_position = self.world_to_grid(
+            msg.pose.pose.position.x,
+            msg.pose.pose.position.y
+        )
     
     def print_information(self):
         elapsed_time = (self.get_clock().now() - self.start_time).nanoseconds / 1e9
         self.get_logger().info(
             f"Elapsed Time: {elapsed_time:.2f} seconds, Total Distance Travelled: {self.total_distance:.2f} meters"
         )
+
+    def publish_frontier_clusters(self, clusters, centroids):
+        marker_array = MarkerArray()
+        marker_id = 0
+
+        # Show frontier clusters
+        for cluster in clusters:
+            m = Marker()
+            m.header.frame_id = "map"
+            m.header.stamp = self.get_clock().now().to_msg()
+            m.ns = "cluster_cells"
+            m.id = marker_id
+            marker_id += 1
+
+            m.type = Marker.SPHERE_LIST
+            m.action = Marker.ADD
+
+            m.scale.x = 0.05   
+            m.scale.y = 0.05
+            m.scale.z = 0.05
+
+            m.color.r = 0.2
+            m.color.g = 0.8
+            m.color.b = 1.0
+            m.color.a = 0.8
+
+            for (r, c) in cluster:
+                p = geometry_msgs.msg.Point()
+                p.x = c * self.map_data.info.resolution + self.map_data.info.origin.position.x
+                p.y = r * self.map_data.info.resolution + self.map_data.info.origin.position.y
+                p.z = 0.05
+                m.points.append(p)
+
+            marker_array.markers.append(m)
+
+        # Show cluster centroid 
+        for (r, c) in centroids:
+            m = Marker()
+            m.header.frame_id = "map"
+            m.header.stamp = self.get_clock().now().to_msg()
+            m.ns = "cluster_centroid"
+            m.id = marker_id
+            marker_id += 1
+
+            m.type = Marker.SPHERE
+            m.action = Marker.ADD
+
+            m.scale.x = 0.15
+            m.scale.y = 0.15
+            m.scale.z = 0.15
+
+            m.color.r = 1.0
+            m.color.g = 0.3
+            m.color.b = 0.2
+            m.color.a = 1.0
+
+            m.pose.position.x = c * self.map_data.info.resolution + self.map_data.info.origin.position.x
+            m.pose.position.y = r * self.map_data.info.resolution + self.map_data.info.origin.position.y
+            m.pose.position.z = 0.1
+
+            marker_array.markers.append(m)
+
+        self.frontier_pub.publish(marker_array)
+
 
     def map_callback(self, msg):
         self.map_data = msg
@@ -178,10 +292,53 @@ class ExplorerNode(Node):
         self.get_logger().info(f"[cluster_frontiers] Found {len(clusters)} clusters.")
         return clusters
 
+    def is_obstacle_region(self, map_array, r, c):
+        """Check whether this frontier centroid is too close to obstacles."""
+        pr = self.patch_radius
+        rows, cols = map_array.shape
+
+        r0 = max(0, r - pr)
+        r1 = min(rows, r + pr + 1)
+        c0 = max(0, c - pr)
+        c1 = min(cols, c + pr + 1)
+
+        patch = map_array[r0:r1, c0:c1]
+
+        total = patch.size
+        obstacles = np.sum(patch == 100)
+
+        obstacle_ratio = obstacles / total
+
+        return obstacle_ratio > self.obstacle_ratio_thresh
+    
+    def is_valid_boundary(self, map_array, r, c):
+        """Check boundary quality (ρ filter)."""
+        pr = self.patch_radius
+        rows, cols = map_array.shape
+
+        r0 = max(0, r - pr)
+        r1 = min(rows, r + pr + 1)
+        c0 = max(0, c - pr)
+        c1 = min(cols, c + pr + 1)
+
+        patch = map_array[r0:r1, c0:c1]
+
+        total = patch.size
+        unknowns = np.sum(patch == -1)
+
+        unknown_ratio = unknowns / total
+
+        # ρ = 1 - 2 * | unknown_ratio - 0.5 |
+        rho = 1 - 2 * abs(unknown_ratio - 0.5)
+
+        return rho >= self.rho_threshold
+
     def find_frontiers(self, map_array):
         """
         Detect frontiers in the occupancy grid map.
         """
+        #### Test ####
+        self.get_logger().info(f'[find_frontiers] Map shape: {map_array.shape}, Free cells: {np.sum(map_array==0)}, Unknown cells: {np.sum(map_array==-1)}')
         frontiers = []
         rows, cols = map_array.shape
 
@@ -200,20 +357,37 @@ class ExplorerNode(Node):
         clusters = self.cluster_frontiers(candidate_mask)
 
         for cluster in clusters:
+            # Filter small clusters
+            if len(cluster) < self.min_cluster_size:
+                self.get_logger().info(f"cluster size={len(cluster)} -> dropped (too small)")
+                continue
+
             # Compute the centroid of the cluster
             if len(cluster) == 0:
                 continue
+
             centroid_r = int(np.mean([pt[0] for pt in cluster]))
             centroid_c = int(np.mean([pt[1] for pt in cluster]))
+
+            # Obstacle filter
+            if self.is_obstacle_region(map_array, centroid_r, centroid_c):
+                self.get_logger().info(f"cluster at ({centroid_r},{centroid_c}) -> dropped (obstacle)")
+                continue
+
+            # Boundary rho filter
+            if not self.is_valid_boundary(map_array, centroid_r, centroid_c):
+                self.get_logger().info(f"cluster at ({centroid_r},{centroid_c}) -> dropped (rho)")
+                continue
+            
             frontiers.append((centroid_r, centroid_c))
 
         self.get_logger().info(
             f"[find_frontiers]Candidate cells: {int(candidate_mask.sum())}, "
             f"clusters found: {len(clusters)}, centroids={len(frontiers)}"
         )
-        return frontiers
+        return clusters, frontiers
 
-    def choose_frontier(self, frontiers, map_array):
+    def choose_frontier(self, clusters, frontiers, map_array):
         """
         Choose a frontier based on a cost/utility function.
         Uses self.config.strategy, self.config.alpha, and self.config.beta directly.
@@ -231,8 +405,12 @@ class ExplorerNode(Node):
                 continue
 
             # Distance to robot (Euclidean in grid coordinates)
-            distance = np.sqrt(
+            # distance = np.sqrt(
+            #     (robot_row - frontier[0])**2 + (robot_col - frontier[1])**2)
+            distance_grid = np.sqrt(
                 (robot_row - frontier[0])**2 + (robot_col - frontier[1])**2)
+
+            distance = distance_grid * self.map_data.info.resolution
 
             if distance < 1e-6:
                 continue  # skip if robot is already at frontier
@@ -259,7 +437,7 @@ class ExplorerNode(Node):
                 chosen_frontier = frontier
 
         if chosen_frontier:
-            self.visited_frontiers.add(chosen_frontier)
+            self.current_target = chosen_frontier
             self.get_logger().info(
                 f"Chosen frontier {chosen_frontier} with score={best_score:.2f} using {strategy} strategy")
         else:
@@ -271,18 +449,19 @@ class ExplorerNode(Node):
         """
         Finish exploration if the number of frontiers is below a threshold.
         """
-        if threshold is None:
-            threshold = getattr(self, 'threshold', 5)
-        if len(frontiers) <= threshold:
-            self.get_logger().info(
-                f"Frontiers below threshold ({threshold}). Exploration complete!")
-            # self.shutdown_robot()
-            # Optionally, stop the timer to halt further exploration
-            self.timer.cancel()
-            return True
+        # if threshold is None:
+        #     threshold = getattr(self, 'threshold', 5)
+        # if len(frontiers) <= threshold:
+        #     self.get_logger().info(
+        #         f"Frontiers below threshold ({threshold}). Exploration complete!")
+        #     # self.shutdown_robot()
+        #     # Optionally, stop the timer to halt further exploration
+        #     self.timer.cancel()
+        #     return True
         return False
 
     def explore(self):
+        
         if self.map_data is None:
             self.get_logger().warning("No map data available")
             return
@@ -292,19 +471,21 @@ class ExplorerNode(Node):
             (self.map_data.info.height, self.map_data.info.width))
 
         # Detect frontiers
-        frontiers = self.find_frontiers(map_array)
+        clusters, centroids = self.find_frontiers(map_array)
+        self.publish_frontier_clusters(clusters, centroids)
+
+        frontiers = centroids
 
         self.get_logger().info(
             f"Exploration: {len(frontiers)} frontier regions detected."
         )
-
-        return
 
         # Finish if frontiers are below threshold
         if self.finish_if_map_complete(frontiers):
             return
 
         chosen_frontier = self.choose_frontier(
+            clusters,
             frontiers,
             map_array
         )
